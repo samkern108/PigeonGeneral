9a20389d76b0735f865623702a3090d6f2e375f7
Sam Kern <samkern108@gmail.com>
2018-01-27 09:30:03 -0800

    UI Icons

--- Assets/Scripts/StagingBird.cs
+++ Assets/Scripts/StagingBird.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StagingBird : MonoBehaviour {
 
 	public Message message;
 	public GameObject target;
 	public GameObject launchPoint;
+
+	public Image actionIcon, directionIcon;
 
 	// First select action
 	// (up for shoot, down for move
@@ -37,7 +40,6 @@ public class StagingBird : MonoBehaviour {
 	}
 
 	private void SelectAction() {
-		Message.Action action;
 		if (Input.GetKeyDown (KeyCode.W)) {
 			UpdateMessageAction(Message.Action.move);
 		}
@@ -55,7 +57,6 @@ public class StagingBird : MonoBehaviour {
 	}
 
 	private void SelectDir() {
-		Message.Dir dir;
 		if (Input.GetKeyDown (KeyCode.W)) {
 			UpdateMessageDir(Message.Dir.up);
 		} else if (Input.GetKeyDown (KeyCode.S)) {
@@ -76,6 +77,31 @@ public class StagingBird : MonoBehaviour {
 	}
 
 	private void Go() {
+
+		switch (message.action) {
+		case Message.Action.shoot:
+			actionIcon.sprite = ResourceManager.self.attackIcon;
+			break;
+		case Message.Action.move:
+			actionIcon.sprite = ResourceManager.self.moveIcon;
+			break;
+		}
+
+		switch (message.dir) {
+		case Message.Dir.down:
+			directionIcon.sprite = ResourceManager.self.downArrow;
+			break;
+		case Message.Dir.up:
+			directionIcon.sprite = ResourceManager.self.upArrow;
+			break;
+		case Message.Dir.right:
+			directionIcon.sprite = ResourceManager.self.rightArrow;
+			break;
+		case Message.Dir.left:
+			directionIcon.sprite = ResourceManager.self.leftArrow;
+			break;
+		}
+
 		gameObject.AddComponent <FlyingBird>();
 		gameObject.GetComponent <FlyingBird>().Initialize(target, launchPoint, message);
 		Destroy (this);
