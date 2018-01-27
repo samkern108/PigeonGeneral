fac53f5c25a48ded7afa461fc8b84c7407ea2ba5
Sam Kern <samkern108@gmail.com>
2018-01-26 22:36:54 -0800

    Message.

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message {
+	public enum Type { move, shoot };
+	public enum Dir { left, right, down, up };
+
+	public Type type;
+	public Dir dir;
+
+	public Message(Type type, Dir dir) {
+		this.type = type;
+		this.dir = dir;
+	}
+}
---
+++ Assets/Scripts/StagingBird.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagingBird : MonoBehaviour {
+
+	public Message message;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
