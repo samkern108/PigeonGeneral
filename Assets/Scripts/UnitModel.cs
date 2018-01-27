3c1a5c02b8634f8c34f63a19679b01df3e71046d
Kurt <kloidl@gmail.com>
2018-01-27 13:41:20 -0800

    multiple UIs

--- Assets/Scripts/UnitModel.cs
+++ Assets/Scripts/UnitModel.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitModel : MonoBehaviour {
-
-	private Color[] playerColors = new Color[] {
+public class Colors {
+	public static Color[] playerColors = new Color[] {
 		new Color(1f, 0f, 0f, 1f),
 		new Color(0f, 1f, 0f, 1f),
 		new Color(0f, 0f, 1f, 1f),
 		new Color(1f, 1f, 1f, 1f),
 	};
+}
+
+public class UnitModel : MonoBehaviour {
 
 	public void Init(int playerIndex) {
 		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
 
-		sr.color = playerColors[playerIndex];
+		sr.color = Colors.playerColors[playerIndex];
 	}
 }
