c35ce3fe610d10ba6f4420d25ed9307a8ce9faa9
Sam Kern <samkern108@gmail.com>
2018-01-28 15:21:24 -0800

    Credits done

---
+++ Assets/CreditsScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour {
+
+	void Start () {
+		Invoke ("ReturnToMain", 35.0f);
+	}
+
+	public void ReturnToMain() {
+		Application.LoadLevel("MainMenu");
+	}
+
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.y += .5f * Time.deltaTime;
+		transform.position = newPosition;
+
+		if (Input.GetKeyDown(KeyCode.W)
+			||	Input.GetKeyDown(KeyCode.Space)
+			||	Input.GetKeyDown(KeyCode.Return)
+			||	Input.GetKeyDown(KeyCode.Escape)) {
+			Application.LoadLevel("MainMenu");
+		}	
+	}
+}
