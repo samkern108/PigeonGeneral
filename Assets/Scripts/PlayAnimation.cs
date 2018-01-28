0021faaeaf7f2e9f1b191315edba9ccf85505bc1
Kurt <kloidl@gmail.com>
2018-01-28 13:58:27 -0800

    temp intro instructions

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+	public bool animateSize;
+	public Vector2 sizeStart;
+	public Vector2 sizeFinish;
+	public float sizeDuration;
+	public Animate.RepeatMode sizeRepeatMode;
+
+	// Use this for initialization
+	void Start () {
+		Animate animate = this.gameObject.GetComponent<Animate>();
+
+		if (animate != null) {
+			if (animateSize) {
+				animate.AnimateToSize(sizeStart, sizeFinish, sizeDuration, sizeRepeatMode);
+			}
+		}
+	}
+}
