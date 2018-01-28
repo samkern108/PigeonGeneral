4ff51ed38f5c118736de7ba99017e087ddddb5bc
Kurt <kloidl@gmail.com>
2018-01-28 11:13:01 -0800

    Added music, audio assets

---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source.clip = clip;
+		source.Play();
+	}
+}
