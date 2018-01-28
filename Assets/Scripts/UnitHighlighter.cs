a4b4cb40e067d6c6628924e8cc5e5e78176e999e
Kurt <kloidl@gmail.com>
2018-01-27 17:29:59 -0800

    unit highlighter WIP

---
+++ Assets/Scripts/UnitHighlighter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHighlighter : MonoBehaviour {
+
+	public GameObject cursor;
+
+	public void SetTarget(Vector3 position) {
+		
+	}
+
+	private void Awake() {
+		GameObject.Instantiate(cursor, new Vector3(-999f, 0f, 0f), Quaternion.identity);
+	}
+}
